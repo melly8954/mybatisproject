35b69a8a9a653900627677f4505da6fb115216bf
haily <tjsdn8963@naver.com>
2024-07-23 14:20:07 +0900

    init add

---
+++ src/main/java/com/melly/mybatisproject/MybatisprojectApplication.java
@@ -0,0 +1,13 @@
+package com.melly.mybatisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melly/mybatisproject/MybatisprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melly.mybatisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
